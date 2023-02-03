c8cfa043ff02b461ea9822c1a4980c7199abecd4
Deniz Cenber <deniz.cenber@student.tuke.sk>
2023-02-03 14:36:57 +0100

    Finished demo

--- REST_EF_demo/Program.cs
+++ REST_EF_demo/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using REST_EF_demo.Data;
+using REST_EF_demo.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Library Service to use it with Dependency Injection in Controllers
+builder.Services.AddTransient<ILibraryService, LibraryService>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 // Register database
 builder.Services.AddDbContext<AppDbContext>(options =>
